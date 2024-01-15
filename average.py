5457caa8347ba7a23458e24c5ff855376238d53d
aronton <aronton0502@gmail.com>
2024-01-15 18:10:05 +0800

    3

--- average.py
+++ average.py
@@ -1,28 +1,33 @@
 import sys
-
+import scriptCreator
 
 task = input("What is the task? (a)submit, (b)resubmit, (c)cancel Jobs: : ")
 print(task)
 parameterlist = {"Spin":None,"L1":None,"L2":None,"delta_L":None,"J1":None,"J2":None,"delta_J":None,\
                  "D1":None,"D2":None,"delta_D":None,"Pdis":None,"bondDim":None,"initialSampleNumber":None,\
                  "finalSampleNumber":None,"sampleDelta":None,"check_Or_Not":None}
+
+parameterlist = {"Spin":None,"L":[None,None,None],"J":[None,None,None],\
+                 "D":[None,None,None],"Pdis":None,"bondDim":None,"initialSampleNumber":None,\
+                 "finalSampleNumber":None,"sampleDelta":None,"check_Or_Not":None}
+
 print("key in parameter in the following format : \n\
 ex : Spin, L1, L2, delta_L, J1, J2, delta_J, D1, D2, delta_D, Pdis, bondDim, initialSampleNumber, finalSampleNumber, sampleDelta, check_Or_Not\n\
 ex : 15(Spin) 64(L) 1.1(J) 0.1(D) 10(Pdis) 40(bondDim) 1(initialSampleNumber) 20(finalSampleNumber) 5(sampleDelta), Y(check_Or_Not)\n")
 
 parameterlist["Spin"]=input("Spin : ")
 
-parameterlist["L1"]=input("L1 : ")
-parameterlist["L2"]=input("L2 : ")
-parameterlist["delta_L"]=input("delta_L : ")
+parameterlist["L"][0]=input("L1 : ")
+parameterlist["L"][1]=input("L2 : ")
+parameterlist["L"][2]=input("delta_L : ")
 
-parameterlist["J1"]=input("J1 : ")
-parameterlist["J2"]=input("J2 : ")
-parameterlist["delta_J"]=input("delta_J : ")
+parameterlist["J"][0]=input("J1 : ")
+parameterlist["J"][1]=input("J2 : ")
+parameterlist["J"][2]=input("delta_J : ")
 
-parameterlist["D1"]=input("D1 : ")
-parameterlist["D2"]=input("D2 : ")
-parameterlist["delta_D"]=input("delta_D : ")
+parameterlist["D"][0]=input("D1 : ")
+parameterlist["D"][1]=input("D2 : ")
+parameterlist["D"][2]=input("delta_D : ")
 
 parameterlist["Pdis"]=input("Pdis : ")
 parameterlist["bondDim"]=input("bondDim : ")
@@ -30,11 +35,16 @@ parameterlist["initialSampleNumber"]=int(input("initialSampleNumber : "))
 parameterlist["finalSampleNumber"]=int(input("finalSampleNumber : "))
 parameterlist["sampleDelta"]=int(input("sampleDelta : "))
 
-mark = parameterlist["check_Or_Not"]
-while mark != "Y" or mark != "N":
-    mark = input("check_Or_Not(Y/N) : ")
-    parameterlist["check_Or_Not"]=mark
+# mark = parameterlist["check_Or_Not"]
+# while mark != "Y" or mark != "N":
+#     mark = input("check_Or_Not(Y/N) : ")
+#     parameterlist["check_Or_Not"]=mark
 
 print(parameterlist,"\n")
 for s in parameterlist:
     print(s," : ",parameterlist[s])
+
+a = scriptCreator.paraList("Jdis",parameterlist["J"])
+
+print(a.toFlo())
+print(a.toStr())
--- scriptCreator.py
+++ scriptCreator.py
@@ -1,48 +1,29 @@
 import re
 
 class paraList:
-    def __init__(self,title,list):
+    def __init__(self,title,numlist):
         print(list)
-        self.x1 = float(list[0])
-        self.x2 = float(list[1])
-        self.delta = float(list[2])
+        self.x1 = float(numlist[0])
+        self.x2 = float(numlist[1])
+        self.dx = float(numlist[2])
         self.title = title
-        
+        self.floatlist = [ round(self.x1 + i*self.dx,2)  for i in range(round((self.x2 - self.x1)/self.dx + 1))]
+        self.floatlist = list(self.floatlist)
     def toStr(self):
-        self.dim1 = str(self.x1)
-        self.dim2 = str(self.x2)
-        self.dimDelta = str(self.delta)
-
-        self.dim1 = re.sub("\.", "", self.dim1)
-        self.dim2 = re.sub("\.", "", self.dim2)
-        self.dimDelta = re.sub("\.", "", self.dimDelta)
-
-        if self.x1 > 0:
-          self.dim1 = self.dim1.ljust(3,"0")
-          self.dim1 = str(self.dim1)
-        else:
-          self.dim1 = re.sub("\-", "", self.dim1)
-          self.dim1 = self.dim1.ljust(3,"0")
-          self.dim1 = "N" + str(self.dim1)
-
-        if self.x2 > 0:
-          self.dim2 = self.dim2.ljust(3,"0")
-          self.dim2 = str(self.dim2)
-        else:
-          self.dim2 = re.sub("\-", "", self.dim2)
-          self.dim2 = self.dim2.ljust(3,"0")
-          self.dim2 = "N" + str(self.dim2)
-          
-        if self.delta > 0:
-          self.dimDelta = self.dimDelta.ljust(3,"0")
-          self.dimDelta = str(self.dimDelta)
-        else:
-          self.dimDelta = re.sub("\-", "", self.dimDelta)
-          self.dimDelta = self.dimDelta.ljust(3,"0")
-          self.dimDelta = "N" + str(self.dimDelta)
-
-        return [self.title + self.dim1, self.title + self.dim2, self.title + self.dimDelta] 
+        strlist = []
+        for x in self.floatlist:
+            self.s = str(x)
+            self.s = re.sub("\.", "", self.s)
+            if x > 0:
+              self.s = self.title + self.s.ljust(3,"0")
+              self.s = str(self.s)
+            else:
+              self.s = re.sub("\-", "", self.s)
+              self.s = self.s.ljust(3,"0")
+              self.s = self.title + "N" + str(self.s)
+            strlist.append(self.s)
+        return strlist
     def toFlo(self):
-        return [self.x1,self.x2,self.dimDelta]
+        return self.floatlist
     def __repr__(self):
-        return f'Point({self.x1}, {self.x2}, {self.delta})'
+        return f'Point({self.x1}, {self.x2}, {self.dx})'
