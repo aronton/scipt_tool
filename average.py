46de019ea47841312fd6e59dab2ba33ca69c10e8
aronton <aronton0502@gmail.com>
2024-01-17 19:21:57 +0800

    seed

--- average.py
+++ average.py
@@ -1,5 +1,7 @@
 import sys
-import scriptCreator
+import os
+
+# import scriptCreator
 
 task = input("What is the task? (a)submit, (b)resubmit, (c)cancel Jobs: : ")
 print(task)
@@ -7,9 +9,9 @@ print(task)
 #                  "D1":None,"D2":None,"delta_D":None,"Pdis":None,"bondDim":None,"initialSampleNumber":None,\
 #                  "finalSampleNumber":None,"sampleDelta":None,"check_Or_Not":None}
 
-parameterlist = {"Spin":None,"L":[None,None,None],"J":[None,None,None],\
-                 "D":[None,None,None],"Pdis":None,"bondDim":None,"initialSampleNumber":None,\
-                 "finalSampleNumber":None,"dx":None,"sampleDelta":None,"check_Or_Not":None}
+parameterlist = {"Spin":None,"L":{"L1":None,"L2":None,"delta_L":None},"J":{"J1":None,"J2":None,"delta_J":None},\
+                 "D":{"D1":None,"D2":None,"delta_D":None},"seed":{"s1":None,"s2":None,"delta_s":None},\
+                 "Pdis":None,"bondDim":None,"dx":None,"check_Or_Not":None}
 
 print("key in parameter in the following format : \n\
 ex : Spin, L1, L2, delta_L, J1, J2, delta_J, D1, D2, delta_D, Pdis, bondDim, initialSampleNumber, finalSampleNumber, sampleDelta, check_Or_Not\n\
@@ -17,24 +19,28 @@ ex : 15(Spin) 64(L) 1.1(J) 0.1(D) 10(Pdis) 40(bondDim) 1(initialSampleNumber) 20
 
 parameterlist["Spin"]=input("Spin : ")
 
-parameterlist["L"][0]=input("L1 : ")
-parameterlist["L"][1]=input("L2 : ")
-parameterlist["L"][2]=input("delta_L : ")
+parameterlist["L"]["L1"]=input("L1 : ")
+parameterlist["L"]["L2"]=input("L2 : ")
+parameterlist["L"]["delta_L"]=input("delta_L : ")
 
-parameterlist["J"][0]=input("J1 : ")
-parameterlist["J"][1]=input("J2 : ")
-parameterlist["J"][2]=input("delta_J : ")
+parameterlist["J"]["J1"]=input("J1 : ")
+parameterlist["J"]["J2"]=input("J2 : ")
+parameterlist["J"]["delta_J"]=input("delta_J : ")
 
-parameterlist["D"][0]=input("D1 : ")
-parameterlist["D"][1]=input("D2 : ")
-parameterlist["D"][2]=input("delta_D : ")
+parameterlist["D"]["D1"]=input("D1 : ")
+parameterlist["D"]["D2"]=input("D2 : ")
+parameterlist["D"]["delta_D"]=input("delta_D : ")
+
+parameterlist["seed"]["s1"]=int(input("initialSampleNumber : "))
+parameterlist["seed"]["s2"]=int(input("finalSampleNumber : "))
+parameterlist["seed"]["delta_s"]=int(input("sampleDelta : "))
 
 parameterlist["Pdis"]=input("Pdis : ")
 parameterlist["dx"]=input("dx : ")
 parameterlist["bondDim"]=input("bondDim : ")
-parameterlist["initialSampleNumber"]=int(input("initialSampleNumber : "))
-parameterlist["finalSampleNumber"]=int(input("finalSampleNumber : "))
-parameterlist["sampleDelta"]=int(input("sampleDelta : "))
+# parameterlist["initialSampleNumber"]=int(input("initialSampleNumber : "))
+# parameterlist["finalSampleNumber"]=int(input("finalSampleNumber : "))
+# parameterlist["sampleDelta"]=int(input("sampleDelta : "))
 
 # mark = parameterlist["check_Or_Not"]
 # while mark != "Y" or mark != "N":
@@ -42,8 +48,49 @@ mark = input("check_Or_Not(Y/N) : ")
 parameterlist["check_Or_Not"]=mark
 
 # for j in 
-print(tSDRG_path)
+# print(tSDRG_path)
 
 print(parameterlist,"\n")
 for s in parameterlist:
     print(s," : ",parameterlist[s])
+
+
+# L=scriptCreator.Lpara("L",parameterlist["L"])
+L=Lpara("L",parameterlist["L"])
+
+L_num = L.toL()
+L_str = L.toStr()
+
+# S=scriptCreator.Spara("Seed",parameterlist["seed"])
+S=Spara("Seed",parameterlist["seed"])
+
+S_num = S.toS()
+S_str = S.toStr()
+
+# print(S_num)
+# print(S_str)
+
+
+# J=scriptCreator.paraList("Jdis",parameterlist["J"])
+J=paraList("Jdis",parameterlist["J"])
+
+J_num = J.toFlo()
+J_str = J.toStr()
+
+# D=scriptCreator.paraList("Dim",parameterlist["D"])
+D=paraList("Dim",parameterlist["D"])
+
+D_num = D.toFlo()
+D_str = D.toStr()
+
+
+
+# for i,m in enumerate(L_str):
+#     for j,n in enumerate(J_str):
+#         for k,o in enumerate(D_str):
+#             for l in range(len(S_num)-1):
+#                 samplelist = list(range(S_num[l],S_num[l+1]))
+#                 for s in samplelist:
+#                     scriptName = m + "_" + n + "_" + o +"_" + str(s)
+#                     print(scriptName)
+                # print(f"{m}_{n}_{o}_{s}")
--- scriptCreator.py
+++ scriptCreator.py
@@ -1,40 +1,9 @@
 import re
 import os
 
-tSDRG_path = os.system("pwd")
-
-# class paraList:
-#     def __init__(self,title,numlist):
-#         print(list)
-#         self.x1 = float(numlist[0])
-#         self.x2 = float(numlist[1])
-#         self.dx = float(numlist[2])
-#         self.title = title
-#         self.floatlist = [ round(self.x1 + i*self.dx,2)  for i in range(round((self.x2 - self.x1)/self.dx + 1))]
-#         self.floatlist = list(self.floatlist)
-#     def toStr(self):
-#         strlist = []
-#         for x in self.floatlist:
-#             self.s = str(x)
-#             self.s = re.sub("\.", "", self.s)
-#             if x > 0:
-#               self.s = self.title + self.s.ljust(3,"0")
-#               self.s = str(self.s)
-#             else:
-#               self.s = re.sub("\-", "", self.s)
-#               self.s = self.s.ljust(3,"0")
-#               self.s = self.title + "N" + str(self.s)
-#             strlist.append(self.s)
-#         return strlist
-#     def toFlo(self):
-#         return self.floatlist
-#     def __repr__(self):
-#         return f'Point({self.x1}, {self.x2}, {self.dx})'
-
-# import re
-
 class paraList:
     def __init__(self,title,numlist):
+        numlist = list(numlist.values())
         self.x1 = float(numlist[0])
         self.x2 = float(numlist[1])
         self.dx = float(numlist[2])
@@ -57,5 +26,56 @@ class paraList:
         return strlist
     def toFlo(self):
         return self.floatlist
+    def __repr__(self):
+        return f'Point({self.x1}, {self.x2}, {self.dx})'
+
+class Lpara(paraList):
+    def __init__(self,title,numlist):
+        numlist = list(numlist.values())
+        self.x1 = int(numlist[0])
+        self.x2 = int(numlist[1])
+        self.dx = int(numlist[2])
+        self.title = title
+    def toL(self):
+        numL = [self.x1 + l*self.dx for l in range(int((self.x2-self.x1)/self.dx) + 1)]
+        return list(numL)   
+    def toStr(self):
+        if self.x2>0:
+            numL = [ self.title + str(self.x1 + l*self.dx) for l in range(int((self.x2-self.x1)/self.dx) + 1)]
+        else:
+            numL = [ self.title + "N" + str((self.x1 + l*self.dx)*-1) for l in range(int((self.x2-self.x1)/self.dx) + 1)]    
+        return list(numL)    
+    def __repr__(self):
+        return f'Point({self.x1}, {self.x2}, {self.dx})'
+
+class Spara(paraList):
+    def __init__(self,title,numlist):
+        numlist = list(numlist.values())
+        self.x1 = int(numlist[0])
+        self.x2 = int(numlist[1])
+        self.dx = int(numlist[2])
+        self.title = title
+    def toS(self):
+        numSeed = []
+        for i in list(range(self.x1 ,self.x2, self.dx)):
+            a = [j for j in list(range(i,i+5))]
+            numSeed.append(a)
+        # print(numSeed)
+        # numL = [self.x1 + l*self.dx for l in range(int((self.x2-self.x1)/self.dx) + 1)]
+        return list(numSeed)   
+    def toStr(self):
+        if self.x2>0:
+            strSeed = []
+            for i in list(range(self.x1 ,self.x2,self.dx)):
+                a = [self.title + str(self.x1 + j*self.dx) for j in list(range(i,i+self.dx))]
+            strSeed.append(a)
+            # numL = [ self.title + str(self.x1 + l*self.dx) for l in range(int((self.x2-self.x1)/self.dx) + 1)]
+        else:
+            strSeed = []
+            for i in list(range(self.x1 ,self.x2,self.dx)):
+                a = [self.title + "N" + str((self.x1 + l*self.dx)*-1) for j in list(range(i,i+self.dx))]
+            strSeed.append(a)
+            # numL = [ self.title + "N" + str((self.x1 + l*self.dx)*-1) for l in range(int((self.x2-self.x1)/self.dx) + 1)]    
+        return list(strSeed)    
     def __repr__(self):
         return f'Point({self.x1}, {self.x2}, {self.dx})'
